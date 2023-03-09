7739b7454b6ec8f346664e2bf18341ba0fcc75cd
JovanMwesigwa <jovanmwesigwa79@gmail.com>
2023-03-09 19:11:27 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,57 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+import { ConnectWallet } from '@thirdweb-dev/react'
+import { useAddress } from '@thirdweb-dev/react'
 
 export default function Home() {
+  const address = useAddress()
   return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+    <div className="w-full h-screen flex-1 flex-col bg-bg-primary">
+      <nav className="w-full flex px-16 flex-row items-center justify-between h-16">
+        <div className="flex flex-1">
+          <h2 className="text-lg text-gray-200">AirDropper</h2>
+        </div>
+        <ul>
+          <li>
+            <ConnectWallet />
+          </li>
+        </ul>
+      </nav>
+
+      <main className="flex flex-1 flex-col items-center justify-center p-6 text-neutral-200">
+        <h1 className="text-4xl font-medium text-neutral-50 ">
+          Sign-up for your AirDrop
         </h1>
 
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/main.jsx</code>, then modify the{" "}
-          <code className="code">src/App.jsx</code> file!
-        </p>
+        <div className="p-6 flex flex-col bg-white shadow-lg my-6 w-1/2 h-1/2 rounded-md">
+          <div className="flex flex-row w-full">
+            <div className="flex flex-1 items-center text-neutral-600 text-lg border-[0.5px] rounded-md px-4 mr-2">
+              {address ? address : 'wallet not connected'}
+            </div>
+            <button className="bg-blue-500 text-white p-2 px-6 rounded-md font-medium">
+              {address ? 'ADD WALLET' : 'CONNECT'}
+            </button>
+          </div>
 
-        <div className="connect">
-          <ConnectWallet />
-        </div>
+          <h3 className="text-sm text-neutral-600 my-4">
+            ✅ Tutorial, Add your current account and press airdrop to get the
+            token!
+          </h3>
 
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+          <div className="flex w-full flex-col border-[0.5px] rounded-t-md  rounded-b-md">
+            <div className="flex flex-row w-full p-4 items-center rounded-t-md  justify-between bg-purple-700 text-white">
+              <h1>Active Addresses</h1>
+            </div>
+            <h2 className="mx-3 my-2 text-neutral-600">
+              0xD2c2591162162Fc57a40bc8a3C9cff0E6dFc9824
+            </h2>
+            <h2 className="mx-3 my-2 text-neutral-600">
+              0xD2c2591162162Fc57a40bc8a3C9cff0E6dFc9824
+            </h2>
+            <h2 className="mx-3 my-2 text-neutral-600">
+              0xD2c2591162162Fc57a40bc8a3C9cff0E6dFc9824
+            </h2>
+          </div>
         </div>
       </main>
     </div>
-  );
+  )
 }
